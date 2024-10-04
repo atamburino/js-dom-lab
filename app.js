281b708411aafbdd4be64304a3cbdaf42fb1f836
Andrew Tamburino <andy@Andrews-Air.lan>
2024-10-03 21:24:32 -0400

    Task 2 Complete

--- app.js
+++ app.js
@@ -6,3 +6,8 @@ const paragraph = document.querySelector('#content p');
 
 console.log(mainTitle);
 console.log(paragraph);
+
+// Updating text content values 
+mainTitle.textContent = 'DOM Manipulation with JavaScript';
+paragraph.textContent = 'Hello, I am Andy and I am learning DOM manipulation!';
+
